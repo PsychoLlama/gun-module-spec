0e6b0dbdc2a42b8176be6d03ed451f6355a1aa19
Jesse Gibson <Jesse_Gibson@me.com>
2015-11-22 00:30:49 -0700

    Tests now easily requirable, new shiny config file for people without jasmine, custom timeouts, gun log squelching (restored after).
    You can now require the tests from your jasmine spec files, instead of doing crazy file path gymnastics.
    
    If you don't have jasmine installed, you can now use the config file in conjunction with "npm test" to run the specs (yet to try it on a machine without jasmine though :o I'll give it a go soon!).
    
    Since I've got a pretty good idea of how long those tests take, I configured the timeout to be more snappy and unforgiving. I put it back after the tests finish, though :)
    Also, gun logs some annoying messages when keys are reused. I squelched those messages.
    
    In summary, the tests can actually be used by people now. Yay, progress!

---
+++ config.js
@@ -0,0 +1,22 @@
+/*jslint node: true */
+
+/*
+	if you don't use jasmine, you can
+	just set the "module" config by
+	passing in a new instance of your module.
+
+	For example:
+
+	var Level = require('gun-level');
+	var module = new Level();
+
+	Once you're done, run "npm test"
+	in this directory and we'll test
+	your module against the spec.
+*/
+
+module.exports = {
+//	module: new (your module here),
+
+//	name: 'your module name here'
+};
--- spec/module.spec.js
+++ spec/module.spec.js
@@ -1,272 +1,281 @@
 /*jslint node: true */
-/*globals describe, it, expect, beforeEach, afterAll, afterEach, beforeAll, jasmine */
+/*globals describe, it, expect, beforeEach, afterEach, beforeAll, afterAll, jasmine */
 'use strict';
 
-var config = require('./config.js');
+var spec, config = require('../config.js');
+
+spec = function (config) {
+	return function () {
+		var scope, gun, test, timeout, log = console.log;
+
+		timeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+		jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000;
+
+		console.log = function () {};
 
 
-var Gun = require('gun');
-jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000;
-
-describe('The ' + config.name, function () {
-	var scope, gun, test, log = console.log;
-	console.log = function () {};
-
-
-	// Fresh instance each test
-	beforeEach(function (done) {
-		scope = Math.floor(Math.random() * 10e15);
-
-		gun = config.extension
-			.get('gun spec test suite').set()
-			.path(scope).set({}, done);
-	});
-
-
-	afterEach(function () {
-		gun.get('gun spec test suite').path(scope).put(null);
-	});
-
-	afterAll(function () {
-		console.log = log;
-	});
-
-
-	describe('put method', function () {
-
-		beforeEach(function () {
-			gun.put({
-				key: 'value'
-			});
-		});
-
-
-
-		it("should permanently save the data", function (done) {
-			gun.path('key').val(function (val) {
-				expect(val).toBe('value');
-				done();
-			});
-		});
-
-		it("should invoke the callback immediately " +
-			"after succeeding or failing",
-			function (done) {
-				gun.put({
-					key: 'new value'
-				}, done);
-			});
-
-		it("should be able to write without error", function (done) {
-			gun.put({
-				key: 'new value'
-			}, function (err) {
-				expect(err).toBe(null);
-				done();
-			});
-		});
-
-		it("should signal errors by sending an " +
-			"object into the error parameter",
-			function (done) {
-				gun.put({
-					key: 'new value'
-				}, function (error) {
-					if (error) {
-						expect(error).toEqual(jasmine.any(Object));
-					}
-					done();
-				});
-			});
-
-		it('should store error values in an "err" ' +
-			'property on the error argument',
-			function (done) {
-				gun.put({
-					key: 'new value'
-				}, function (error) {
-					if (error) {
-						expect(error.err).toBeTruthy();
-					}
-					done();
-				});
-			});
-
-		it('should signal success by sending ' +
-			'an object in the success parameter',
-			function (done) {
-				gun.put({
-					key: 'new value'
-				}, function (error, success) {
-					if (!error) {
-						expect(success).toEqual(jasmine.any(Object));
-					}
-					done();
-				});
-			});
-
-		it('should store the success status in ' +
-			'an "ok" property of the success parameter',
-			function (done) {
-				gun.put({
-					key: 'new value'
-				}, function (error, success) {
-					if (!error && success) {
-						expect(success.ok).toBeTruthy();
-					}
-					done();
-				});
-			});
-
-	});
-
-
-
-
-
-
-
-	describe('get method', function () {
-
+		// Fresh instance each test
 		beforeEach(function (done) {
-			gun.path('get-test').put({
-				success: true,
-				object: {
-					prop: 'value'
-				}
-			}).key('get-test-key', done);
+			scope = Math.floor(Math.random() * 10e15);
+
+			gun = config.module
+				.get('gun spec test suite').set()
+				.path(scope).set({}, done);
 		});
 
-		it("should be able to find data by it's soul", function (done) {
-			gun.path('get-test', function (error, graph) {
-				expect(graph).toEqual(jasmine.any(Object));
-				done();
+
+		afterEach(function () {
+			gun.get('gun spec test suite').path(scope).put(null);
+		});
+
+		afterAll(function () {
+			console.log = log;
+			jasmine.DEFAULT_TIMEOUT_INTERVAL = timeout;
+		});
+
+
+		describe('put method', function () {
+
+			beforeEach(function () {
+				gun.put({
+					key: 'value'
+				});
 			});
+
+
+
+			it("should permanently save the data", function (done) {
+				gun.path('key').val(function (val) {
+					expect(val).toBe('value');
+					done();
+				});
+			});
+
+			it("should invoke the callback immediately " +
+				"after succeeding or failing",
+				function (done) {
+					gun.put({
+						key: 'new value'
+					}, done);
+				});
+
+			it("should be able to write without error", function (done) {
+				gun.put({
+					key: 'new value'
+				}, function (err) {
+					expect(err).toBe(null);
+					done();
+				});
+			});
+
+			it("should signal errors by sending an " +
+				"object into the error parameter",
+				function (done) {
+					gun.put({
+						key: 'new value'
+					}, function (error) {
+						if (error) {
+							expect(error).toEqual(jasmine.any(Object));
+						}
+						done();
+					});
+				});
+
+			it('should store error values in an "err" ' +
+				'property on the error argument',
+				function (done) {
+					gun.put({
+						key: 'new value'
+					}, function (error) {
+						if (error) {
+							expect(error.err).toBeTruthy();
+						}
+						done();
+					});
+				});
+
+			it('should signal success by sending ' +
+				'an object in the success parameter',
+				function (done) {
+					gun.put({
+						key: 'new value'
+					}, function (error, success) {
+						if (!error) {
+							expect(success).toEqual(jasmine.any(Object));
+						}
+						done();
+					});
+				});
+
+			it('should store the success status in ' +
+				'an "ok" property of the success parameter',
+				function (done) {
+					gun.put({
+						key: 'new value'
+					}, function (error, success) {
+						if (!error && success) {
+							expect(success.ok).toBeTruthy();
+						}
+						done();
+					});
+				});
+
 		});
 
-		it("should be able to find data by using it's key", function (done) {
-			gun.get('get-test-key', function (error, graph) {
-				if (!error) {
+
+
+
+
+
+
+		describe('get method', function () {
+
+			beforeEach(function (done) {
+				gun.path('get-test').put({
+					success: true,
+					object: {
+						prop: 'value'
+					}
+				}).key('get-test-key', done);
+			});
+
+			it("should be able to find data by it's soul", function (done) {
+				gun.path('get-test', function (error, graph) {
 					expect(graph).toEqual(jasmine.any(Object));
-				}
-				done();
-			});
-		});
-
-		it("should terminate the connection stream when it's finished", function (done) {
-			gun.get('get-test-key', done);
-		});
-
-		it("should terminate the object stream when it's finished", function (done) {
-			gun.get('get-test-key').val(done);
-		});
-
-		it('should not throw an error for existing keys', function (done) {
-			gun.get('get-test-key', function (error) {
-				expect(error).toBe(null);
-				done();
-			});
-		});
-
-		it('should not error for empty collections', function (done) {
-			gun.get("this key doesn't exist", function (err) {
-				expect(err).toBe(null);
-				done();
-			});
-		});
-
-	});
-
-
-
-
-
-	describe('key method', function () {
-
-		beforeEach(function (done) {
-			gun.path('to data').set().path('hidden obscurely').put({
-				prop: 'my context',
-				object: {
-					data: true,
-					success: true
-				}
-			}).key('first key').key('second key', done);
-		});
-
-
-
-		it('should persist the key name that points to the graph', function (done) {
-			gun.get('first key').path('prop').val(function (val) {
-				expect(val).toBe('my context');
-				done();
-			});
-		});
-
-
-		it('should allow several keys to point to the same souls', function (done) {
-			gun.get('second key').path('prop').val(function (val) {
-				expect(val).toBe('my context');
-				done();
-			});
-		});
-
-		it("should invoke the callback immediately " +
-			"after succeeding or failing",
-			function (done) {
-				gun.key('key-test-name', done);
-			});
-
-		it("should not fail to persist keys", function (done) {
-			gun.key('key-test-name', function (err) {
-				expect(err).toBe(null);
-				done();
-			});
-		});
-
-		it("should signal errors by sending an " +
-			"object into the error parameter",
-			function (done) {
-				gun.key('key-test-name', function (error) {
-					if (error) {
-						expect(error).toEqual(jasmine.any(Object));
-					}
 					done();
 				});
 			});
 
-		it('should store error values in an "err" ' +
-			'property on the error argument',
-			function (done) {
-				gun.key('key-test-name', function (error) {
-					if (error) {
-						expect(error.err).toBeTruthy();
-					}
-					done();
-				});
-			});
-
-		it('should signal success by sending ' +
-			'an object in the success parameter',
-			function (done) {
-				gun.key('key-test-name', function (error, success) {
+			it("should be able to find data by using it's key", function (done) {
+				gun.get('get-test-key', function (error, graph) {
 					if (!error) {
-						expect(success).toEqual(jasmine.any(Object));
+						expect(graph).toEqual(jasmine.any(Object));
 					}
 					done();
 				});
 			});
 
-		it('should store the success status in ' +
-			'an "ok" property of the success parameter',
-			function (done) {
-				gun.key('key-test-name', function (error, success) {
-					if (!error && success) {
-						expect(success.ok).toBeTruthy();
-					}
+			it("should terminate the connection stream when it's finished", function (done) {
+				gun.get('get-test-key', done);
+			});
+
+			it("should terminate the object stream when it's finished", function (done) {
+				gun.get('get-test-key').val(done);
+			});
+
+			it('should not throw an error for existing keys', function (done) {
+				gun.get('get-test-key', function (error) {
+					expect(error).toBe(null);
 					done();
 				});
 			});
 
-	});
+			it('should not error for empty collections', function (done) {
+				gun.get("this key doesn't exist", function (err) {
+					expect(err).toBe(null);
+					done();
+				});
+			});
 
-});
+		});
+
+
+
+
+
+		describe('key method', function () {
+
+			beforeEach(function (done) {
+				gun.path('to data').set().path('hidden obscurely').put({
+					prop: 'my context',
+					object: {
+						data: true,
+						success: true
+					}
+				}).key('first key').key('second key', done);
+			});
+
+
+
+			it('should persist the key name that points to the graph', function (done) {
+				gun.get('first key').path('prop').val(function (val) {
+					expect(val).toBe('my context');
+					done();
+				});
+			});
+
+
+			it('should allow several keys to point to the same souls', function (done) {
+				gun.get('second key').path('prop').val(function (val) {
+					expect(val).toBe('my context');
+					done();
+				});
+			});
+
+			it("should invoke the callback immediately " +
+				"after succeeding or failing",
+				function (done) {
+					gun.key('key-test-name', done);
+				});
+
+			it("should not fail to persist keys", function (done) {
+				gun.key('key-test-name', function (err) {
+					expect(err).toBe(null);
+					done();
+				});
+			});
+
+			it("should signal errors by sending an " +
+				"object into the error parameter",
+				function (done) {
+					gun.key('key-test-name', function (error) {
+						if (error) {
+							expect(error).toEqual(jasmine.any(Object));
+						}
+						done();
+					});
+				});
+
+			it('should store error values in an "err" ' +
+				'property on the error argument',
+				function (done) {
+					gun.key('key-test-name', function (error) {
+						if (error) {
+							expect(error.err).toBeTruthy();
+						}
+						done();
+					});
+				});
+
+			it('should signal success by sending ' +
+				'an object in the success parameter',
+				function (done) {
+					gun.key('key-test-name', function (error, success) {
+						if (!error) {
+							expect(success).toEqual(jasmine.any(Object));
+						}
+						done();
+					});
+				});
+
+			it('should store the success status in ' +
+				'an "ok" property of the success parameter',
+				function (done) {
+					gun.key('key-test-name', function (error, success) {
+						if (!error && success) {
+							expect(success.ok).toBeTruthy();
+						}
+						done();
+					});
+				});
+
+		});
+
+	};
+};
+
+if (config.module) {
+	describe(config.name || '', spec(config));
+}
+
+module.exports = spec;
